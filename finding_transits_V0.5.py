359b5780cb63bdcf6dffe624ade4c487f30446ce
Derod Deal <dealderod@gmail.com>
2021-03-29 03:47:48 -0400

    Adding phase and period folding (notworking)
    In progress

--- finding_transits_V0.5.py
+++ finding_transits_V0.5.py
@@ -17,6 +17,7 @@ xval = np.array(data.iloc[:,0])
 
 yval = np.array(data.iloc[:,1])
 
+print(xval)
 sp = pyspeckit.Spectrum(data=yval, xarr=xval)
 
 #
@@ -40,6 +41,7 @@ def transit_std(flux, t1, t2):
 
 
 #Plot the First Transit
+'''
 import pylab as pl
 
 fig = pl.figure(figsize=(8,6))
@@ -48,15 +50,17 @@ sp.plotter(color='black',xmin=0.0e7, xmax=0.010e7 ,ymin=0.99900, ymax=1.00075,fi
 pl.xlabel('Time (s)', fontname="Times New Roman")
 pl.ylabel('Normalized Flux',fontname="Times New Roman")
 
-sp.baseline(interactive=True, subtract=False)
+# sp.baseline(interactive=True, subtract=False)
 
-sp.specfit(interactive=True)
-
+# sp.specfit(interactive=True)
+'''
 
 #Overplot Transits with Phase shifts
 
 '''  
 Helpful Resources
+
+# Use this link from astropy: https://docs.astropy.org/en/stable/timeseries/
 
 https://doi.org/10.1051/0004-6361/201834672  
 https://www.qmul.ac.uk/spa/media/school-of-physics/outreach/research-in-schools/PlanetHuntingWithPythonManual-StudentVersion-UpdatedDataDownloadInstructions.pdf
@@ -70,8 +74,57 @@ Notes to self
  - Plot the points of observation and gaussians for each plot 
 
 '''
+import pylab as pl
+from astropy.timeseries import TimeSeries
+from astropy.utils.data import get_pkg_data_filename
+
+from astropy.timeseries import BinnedTimeSeries
+
+'''
+From astropy...
+
+Parameters
+
+    datanumpy ndarray, dict, list, Table, or table-like object, optional
+
+        Data to initialize time series. This does not need to contain the times,
+        which can be provided separately, but if it does contain the times they 
+        should be in a column called 'time' to be automatically recognized.
+        
+    fold(period=None, epoch_time=None, epoch_phase=0, wrap_phase=None,
+         normalize_phase=False)[source]
+'''
+'''
+binned_filename = get_pkg_data_filename('/Users/dealderod/Documents/Number10_1.csv',
+                                         package='astropy.timeseries.tests')
+ts = BinnedTimeSeries.read('/Users/dealderod/Documents/Number10_1.csv', format='ascii.csv', time_bin_start_column='time_start',
+                           time_bin_size_column='bin_size',
+                           time_bin_size_unit=u.s)
+'''
+data = np.concatenate([xval,yval])
 
 orbital_period = 597.8 #days
+
+
+ts = TimeSeries(time_start='2016-03-22T12:30:31',
+                time_delta=7.1889609587E+01 * u.s,
+                data={'normalized flux': yval})
+
+ts_folded = ts.fold(period=51649920*u.s) 
+pl.plot(ts_folded.time.jd, ts_folded['normalized flux'], 'k.', markersize=1) 
+pl.show()
+print(ts)
+'''
+sampled_filename = get_pkg_data_filename('/Users/dealderod/Documents/Number10_1.csv',
+                                         package='astropy.timeseries.tests')
+
+ts = TimeSeries.read(sampled_filename, format='ascii.csv',
+                     time_column='Date')
+
+orbital_period = 597.8 #days
+'''
+
+
 
 def gaussian(xval, a, FWHM, rest_f):
     return (a*(np.exp(-np.log(2)*(((xval-rest_f)**2)/((0.5*FWHM)**2)))))
